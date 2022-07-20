5abc3b2abaa955a0a1737d8e006df805fefc1e63
Rafhael Gomes <rafhaelgomes@iMac-de-Rafhael.local>
2022-07-20 16:34:03 -0300

    Requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Title from './Title';
+import p from '../data/planets';
+import Pla from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {p.map((e, i) => <Pla key={ i } planetName={ e.name } planetImage={ e.image } />)}
       </div>
     );
   }
